6345363d30c3a8e6881ce110ba06b1bc41c0f7af
YasirHasn9 <hassanyasir223@gmail.com>
2020-04-20 18:46:32 -0700

    delete an action

--- routers/actionsRouter.js
+++ routers/actionsRouter.js
@@ -3,11 +3,36 @@ const actions = require("../data/helpers/actionModel");
 const { validateProjectId } = require("../middlewars/validate");
 const router = express.Router({ mergeParams: true });
 
-router.get("/", validateProjectId(), async (req, res) => {
+router.get("/", validateProjectId(), async (req, res, next) => {
   try {
     const projectActions = await actions.get(req.params.project_id);
     res.status(200).json({ message: projectActions });
-  } catch (err) {}
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/", validateProjectId(), async (req, res, next) => {
+  try {
+    let newAction = {
+      project_id: req.params.id,
+      description: req.body.description,
+      notes: req.body.notes
+    };
+    const newProject = await actions.insert(newAction);
+    res.json(newProject);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/:action_id", validateProjectId(), async (req, res, next) => {
+  try {
+    let deleteAction = await actions.remove(req.params.action_id);
+    res.json(deleteAction);
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
